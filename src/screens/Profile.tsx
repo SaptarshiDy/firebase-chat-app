1ee20aae3e5c3b0058f736a73802c3ff465f4c66
Saptarshi Dey <saptarshi7047@gmail.com>
2023-10-11 22:55:50 +0530

    Profile screen basic

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
-import { Animated, Text, TouchableOpacity, View } from 'react-native';
-import BaseLayout from '../Layouts/BaseLayout';
+import { Image, View, Text } from 'react-native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
 
 const Profile = () => {
+    const auth = useAppSelector(state => state.auth);
+
     return (
         <View className='p-4'>
-            <Text>Profile</Text>
+            <View className='flex items-center justify-center gap-y-4 shadow-lg bg-blue-200 rounded-t-lg pb-4 mt-4'>
+                <View className='rounded-full border-2 border-indigo-600 p-1'>
+                    <Image
+                        style={{
+                            width: 100,
+                            height: 100,
+                            borderRadius: 50,
+                        }}
+                        source={{
+                            uri: auth.user.photo,
+                        }}
+                    />
+                </View>
+                <View className='items-center'>
+                    <Text className='text-2xl font-black'>{auth.user.name}</Text>
+                    <Text className='text-lg'>{auth.user.email}</Text>
+                </View>
+            </View>
+            <View className='flex gap-y-4 shadow-lg bg-blue-200 rounded-b-lg pb-4 mt-4'>
+
+            </View>
         </View>
     );
 }
